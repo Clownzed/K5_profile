ddcd2b0096e542c855fa2d44b3a8a6347a90395a
cnorzen <binhdtph15131@fpt.edu.vn>
2023-10-30 19:31:58 +0700

    init profile

---
+++ app.js
@@ -0,0 +1,7 @@
+function editInfo() {
+    document.getElementById("hoTen").contentEditable = true;
+    document.getElementById("tuoi").contentEditable = true;
+    document.getElementById("queQuan").contentEditable = true;
+    document.getElementById("dinhHuong").contentEditable = true;
+    document.getElementById("ngonNgu").contentEditable = true;
+}
